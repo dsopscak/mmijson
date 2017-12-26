68aaf83d2f317d1353283001601d2fc6dbc05fa5
Skip Sopscak <dsopscak@gmail.com>
2017-12-25 19:34:45 -0600

    Flesh out implementation data structures some more

--- json.c
+++ json.c
@@ -2,32 +2,54 @@
 
 #include "json.h"
 
-typedef struct JSON_MAP
-    {
-    } JSON_MAP;
-
-typedef struct JSON_ARRAY
-    {
-    } JSON_ARRAY;
+typedef struct MAP MAP;
+typedef struct MAP_NODE MAP_NODE;
+typedef struct ARRAY ARRAY;
+typedef struct ARRAY_NODE ARRAY_NODE;
 
 typedef struct DATA
-{
-  enum { map, array, string, number } type;
-  union 
-      {
-      char *string;
-      JSON_MAP *map;
-      JSON_ARRAY *array;
-      } data;
-} DATA;
+    {
+    enum { map, array, string, number } type;
+    union 
+        {
+        char *string;
+        MAP *map;
+        ARRAY *array;
+        } data;
+    } DATA;
+
+struct ARRAY_NODE
+    {
+    DATA *data;
+    ARRAY_NODE *next;
+    };
+
+struct MAP_NODE
+    {
+    char *key;
+    DATA *data;
+    MAP_NODE *next;
+    };
+
+struct MAP
+    {
+    MAP_NODE *head;
+    };
+
+
+struct ARRAY
+    {
+    ARRAY_NODE *head;
+    };
+
 
 struct JSON
-{
-
-};
+    {
+    MAP *map;
+    };
 
 JSON *init_json_file(FILE *f)
-{
+    {
 
-  return NULL;
-}
+    return NULL;
+    }
