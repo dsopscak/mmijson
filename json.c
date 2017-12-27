b9106c92e2e940c7ad6afd6dbde039e04c179bba
Skip Sopscak <dsopscak@gmail.com>
2017-12-26 22:02:21 -0600

    Start on parsing support

--- json.c
+++ json.c
@@ -3,6 +3,9 @@
 #include "json.h"
 #include "string.h"
 #include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <stdbool.h>
 
 typedef struct MAP MAP;
 typedef struct MAP_NODE MAP_NODE;
@@ -19,6 +22,29 @@ typedef struct DATA
         ARRAY *array;
         } data;
     } DATA;
+
+static char *get_json_string_copy(const char *s)
+    {
+    char *copy = get_json_string(strlen(s));
+    strcpy(copy, s);
+    return copy;
+    }
+
+static DATA *create_data_node_string(const char *s)
+    {
+    DATA *data = malloc(sizeof(DATA));
+    data->type = string;
+    data->data.string = get_json_string_copy(s);
+    return data;
+    }
+
+static DATA *create_data_node_number(char *s)
+    {
+    DATA *data = malloc(sizeof(DATA));
+    data->type = number;
+    data->data.string = get_json_string_copy(s);
+    return data;
+    }
 
 struct ARRAY_NODE
     {
@@ -38,12 +64,81 @@ struct MAP
     MAP_NODE *head;
     };
 
+static DATA *create_data_node_map()
+    {
+    DATA *data = malloc(sizeof(DATA));
+    data->type = map;
+    data->data.map = malloc(sizeof(MAP));
+    data->data.map->head = NULL;
+    return data;
+    }
+
+static void put_data_map(MAP *map, const char *key, DATA *data)
+    {
+    if (!map->head)
+        {
+        map->head = malloc(sizeof(MAP_NODE));
+        map->head->key = get_json_string_copy(key);
+        map->head->data = data;
+        map->head->next = NULL;
+        }
+    else
+        {
+        MAP_NODE *p = map->head;
+        while (true)
+            {
+            if (strcmp(p->key, key))
+                {
+                if (p->next)
+                    p = p->next;
+                else
+                    {
+                    p->next = malloc(sizeof(MAP_NODE));
+                    p->next->key = get_json_string_copy(key);
+                    p->next->data = data;
+                    break;
+                    }
+                }
+            else
+                {
+                p->data = data;
+                break;
+                }
+            }
+        }
+    }
+
 
 struct ARRAY
     {
     ARRAY_NODE *head;
+    ARRAY_NODE *tail;
     };
 
+static DATA *create_data_node_array(char *s)
+    {
+    DATA *data = malloc(sizeof(DATA));
+    data->type = array;
+    data->data.array = malloc(sizeof(ARRAY));
+    data->data.array->head = NULL;
+    data->data.array->tail = NULL;
+    return data;
+    }
+
+static void put_data_array(ARRAY *array, DATA *data)
+    {
+    ARRAY_NODE *node = malloc(sizeof(ARRAY_NODE));
+    node->data = data;
+    node->next = NULL;
+    if (array->head)
+        {
+        assert(!array->tail->next);
+        array->tail->next = node;
+        array->tail = node;
+        }
+    else
+        array->head = array->tail = node;
+    }
 
 struct JSON
     {
@@ -63,12 +158,23 @@ static JSON *create_json(void)
     return json;
     }
 
+static void parse_into_array(FILE *f, ARRAY *array)
+    {
+    }
+
 static void parse_into_map(FILE *f, MAP *map)
     {
+
     }
 
 JSON *init_json_file(FILE *f)
     {
+    int c;
+    while ((c = fgetc(f)) != EOF)
+        if (c == '{') break;
+    if (c != '{')
+        return NULL;
+
     JSON *json = create_json();
 
     parse_into_map(f, json->map);
